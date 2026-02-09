ca0d824d8e6408b9241950fd96103bf249a4cd59
JoRE13 <joi.buland@gmail.com>
2026-02-09 21:59:03 +0000

    fixes

--- assignment3/agentic.py
+++ assignment3/agentic.py
@@ -4,11 +4,14 @@ from PIL import Image
 from pathlib import Path
 import time
 import pandas as pd
+import json
+import re
 
 
 client = genai.Client()
 
 df = pd.read_csv('assignment3.csv')
+
 
 
 with open("./prompts/v1.txt", "r") as file:
@@ -31,15 +34,37 @@ def call_model_with_retry(image,mode, max_retries=5):
             print(f"Server busy, retrying in {wait}s...")
             time.sleep(wait)
 
+result_df = pd.DataFrame()
 
 for row in df.itertuples():
     file_path = f"./img/{row.image}"
     print(f"Processing {file_path}")
+
     with Image.open(file_path) as image:
         try:
             result = call_model_with_retry(image, row.mode)
-            print(result)
+            if result.startswith("```"):
+                result = re.sub(r"^```(?:json)?\s*", "", result)
+                result = re.sub(r"\s*```$", "", result).strip()
+
+            # convert model response (string) to dict
+            result_json = json.loads(result)
+
+            # add extra fields
+            result_json["file_path"] = file_path
+            result_json["expected_verdict"] = row.expected_verdict
+
+            # append to dataframe
+            result_df = pd.concat(
+                [result_df, pd.DataFrame([result_json])],
+                ignore_index=True
+            )
+
         except Exception as e:
             print(f"Failed on {file_path}: {e}")
-    # pacing between requests helps a lot
+
     time.sleep(1)
+
+print(result_df)
+result_df.to_csv('results_v1.csv')
+
